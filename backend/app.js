9285b29573a91d9c34244bc7db6c73d95ea18fb4
wfiene <walkerfiene@gmail.com>
2022-11-16 12:45:03 -0500

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -18,15 +18,15 @@ app.use(express.json());
 
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
-  }
-  
-  // helmet helps set a variety of headers to better secure your app
-  app.use(
-    helmet.crossOriginResourcePolicy({ 
-      policy: "cross-origin" 
-    })
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({ 
+    policy: "cross-origin" 
+  })
   );
   
   // Set the _csrf token and create req.csrfToken method
@@ -38,13 +38,44 @@ if (!isProduction) {
         httpOnly: true
       }
     })
-  );
+    );
+    
+    // backend/app.js
+    const routes = require('./routes');
+    app.use(routes); // Connect all the routes
+    
+    // Catch unhandled requests and forward to error handler.
+    app.use((_req, _res, next) => {
+      const err = new Error("The requested resource couldn't be found.");
+      err.title = "Resource Not Found";
+      err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
 
-  // backend/app.js
-const routes = require('./routes');
+// Process sequelize errors
+const { ValidationError } = require('sequelize');
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
 
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 // ...
 
-app.use(routes); // Connect all the routes
 
 module.exports = app;
