a25f75dae2ffcf9ef59c2dcfb48647eff8a67855
wfiene <walkerfiene@gmail.com>
2022-11-28 13:14:22 -0500

    Options Object Update

--- backend/db/migrations/20221122220647-create-spot.js
+++ backend/db/migrations/20221122220647-create-spot.js
@@ -67,7 +67,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: new Date()
       },
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Spots"
